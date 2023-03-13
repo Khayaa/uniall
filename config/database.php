f4eeecbb38ea59869beea87f92d5b3dbf838b90d
Khaya Ngwenya <khaya.developer@gmail.com>
2023-03-13 05:58:25 +0200

    Update database.php

--- config/database.php
+++ config/database.php
@@ -87,7 +87,7 @@ return [
             'password' => env('DB_PASSWORD', ''),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
-            'prefix'    => 'wp_',
+            'prefix'    => 'tgR5C2n_',
             'strict'    => false,
             'engine'    => null,
         ],
